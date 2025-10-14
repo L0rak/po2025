8eacdbd2e8608e453b4a5d90f31a4a6aa00df388
Karol <karol.brodowicz@gmail.com>
2025-10-14 19:10:08 +0200

    Added proper module and loop testing

---
+++ Lab2/src/Lotto.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Lotto {
+
+    public static void main(String[] args) {
+
+        long starting= System.currentTimeMillis();
+
+        ArrayList<Integer> Typy = new ArrayList<Integer>();
+
+        for(int i = 0; i<6; i++) {
+            Typy.add(Integer.parseInt(args[i]));
+        }
+
+        System.out.println("Typy uzytkownika: " + Typy);
+
+        int trafienia = 0;
+        int liczbaprzejsc = 0;
+
+        while(trafienia != 6) {
+            trafienia = 0;
+
+            Random random = new Random();
+            ArrayList<Integer> wyniki = new ArrayList<Integer>();
+
+            for(int i = 0; i<6; i++) {
+                wyniki.add(random.nextInt(49));
+            }
+//            System.out.println("Wylosowane liczby: " + wyniki);
+
+            for (Integer typ : Typy) {
+                if (wyniki.contains(typ)) {
+                    trafienia++;
+                }
+            }
+
+            liczbaprzejsc++;
+
+//            System.out.println("Liczba trafień: " + trafienia);
+
+        }
+
+        long ending=System.currentTimeMillis();
+        System.out.println("Ile szukalo: "+ (ending-starting) + "ms");
+        System.out.println("Ile razy przeszla petla: "+ liczbaprzejsc);
+
+    }
+
+}
